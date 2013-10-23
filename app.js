5d97527a40fc26468d66ac00987500a7c693b494
Atom Raiff <atom.raiff@gmail.com>
2013-10-23 14:28:28 -0500

    Added the fully functional route: '/api/stockbyname/:name'. Also, it's important to note that some of the API call names have been changed so whoever is using it at the moment will need to change.

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var port = process.env.PORT || 5000, express = require("express"),
-    app = express(), yql = require("yql");
+    app = express(), yql = require("yql"), fs = require("fs");
+var nameList = [];
 
 app.set("view engine", "ejs");
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(__dirname + '/views'));
 
+function init(){
+  var symbols = fs.readFileSync("symbols.txt","utf8");
+  symbols = symbols.split("\n");
+  for(line in symbols){
+    if (symbols.hasOwnProperty(line) && symbols[line]) {
+      var pair = symbols[line].split("\t");
+      pair[1] = pair[1].slice(0,pair[1].length-2);
+      nameList.push([pair[1],pair[0]]);
+    }
+  }
+}
 
 
 // Homepage
@@ -25,8 +37,18 @@ app.post("/test/stock",function(req,res){
   });
 });
 
-app.get("/api/stock/:symbol", function(req, res){
-  getStockData(req.params['symbol'], "*", function(stock){  
+app.get("/api/stockbysymbol/:symbol", function(req, res){
+  getStockBySymbol(req.params['symbol'], "*", function(stock){  
+//    console.log(stock);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify(stock));
+    res.end();
+  });
+});
+
+app.get("/api/stockbyname/:name", function(req, res){
+  var symbols = symbolsContainingName(req.params['name']).join();
+  getStockBySymbol(symbols, "*", function(stock){  
 //    console.log(stock);
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(stock));
@@ -35,7 +57,7 @@ app.get("/api/stock/:symbol", function(req, res){
 });
 
 app.get("/api/stock/:symbol/:field", function(req, res){
-	getStockData(req.params['symbol'], req.params['field'], function(stock) {
+	getStockBySymbol(req.params['symbol'], req.params['field'], function(stock) {
 		console.log(stock);
 		res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(stock));
@@ -52,7 +74,7 @@ app.get("/api/history/:symbol/:startDate/:endDate", function(req, res){
   });
 });
 
-function getStockData(symbol, field, callback){
+function getStockBySymbol(symbol, field, callback){
 //  console.log(symbol);
   new yql.exec("select " + field + " from yahoo.finance.quote where (symbol = @symbol)", function(yqlResponse){
   	if (yqlResponse.query.results != null)
@@ -70,5 +92,19 @@ function getStockHistory(symbol, field, startDate, endDate, callback){
 , {"symbol": symbol, "startDate": startDate, "endDate": endDate})
 }
 
+function symbolsContainingName(str){
+  var regex = new RegExp(str, "i", "g");
+  var results = [];
+  for(pair in nameList){
+    if(nameList.hasOwnProperty(pair)){
+      if(regex.test(nameList[pair][0])){
+          results.push(nameList[pair][1]);
+        }
+    }
+  }
+  return results;
+}
+
+init();
 app.listen(port);
 console.log("App started...");
